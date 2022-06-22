a0bf0742e268b3f2b5b97151120e7a220e8a9f48
Dinko99 <dinkodin99@gmail.com>
2022-06-22 22:03:08 +0200

    Updated index.js and added index.html

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('ajmo git');
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+function addMultipleNumbers(...params) {
+  return params.reduce((acc, cur) => acc + cur);
+}
+
+console.log(addTwoNumbers(1, 2));
+console.log(addMultipleNumbers(1, 2, 3, 4, 5, 6, 7));
